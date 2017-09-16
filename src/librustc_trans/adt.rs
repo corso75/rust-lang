17a49f02410c128cb8fac53553e2fa102090b269
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2017-09-16 16:39:53 +0300

    rustc: move CEnum's signedness into Primitive::Int.

--- src/librustc_trans/adt.rs
+++ src/librustc_trans/adt.rs
@@ -225,9 +225,3 @@ pub fn struct_llfields<'a, 'tcx>(cx: &CrateContext<'a, 'tcx>,
     result
 }
 
-pub fn is_discr_signed<'tcx>(l: &layout::Layout) -> bool {
-    match *l {
-        layout::CEnum { signed, .. }=> signed,
-        _ => false,
-    }
-}
