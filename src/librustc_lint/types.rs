04645e511c0f26d78690eb5b03efee182bb16fd5
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2017-09-17 19:34:28 +0300

    rustc: store CachedLayout for each variant of enum Layout's instead of Struct.

--- src/librustc_lint/types.rs
+++ src/librustc_lint/types.rs
@@ -756,7 +756,7 @@ impl<'a, 'tcx> LateLintPass<'a, 'tcx> for VariantSizeDifferences {
                         .zip(variants)
                         .map(|(variant, variant_layout)| {
                             // Subtract the size of the enum discriminant
-                            let bytes = variant_layout.min_size
+                            let bytes = variant_layout.abi.size(cx.tcx)
                                 .bytes()
                                 .saturating_sub(discr_size);
 
